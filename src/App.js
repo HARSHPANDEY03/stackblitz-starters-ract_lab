8c370d06c8968bbaaa72627d50b2871e0d0a80ba
HARSHPANDEY03 <95539409+HARSHPANDEY03@users.noreply.github.com>
2023-08-14 13:19:19 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './style.css';
 import { useState } from 'react';
 export default function App() {
-  const [name, setName] = useState('kc');
+  const [name, setName] = useState('Harsh');
   return (
     <div>
       <h1>hello {name}</h1>
       <p>Start editing to see some magic happen :)</p>
-      <button onClick={() => setName('Students')}> Change Name</button>
+      <button onClick={() => setName('Datta')}> Change Name</button>
     </div>
   );
 }
